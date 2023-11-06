d7c4f2fd2cf27bb802205a5691c0d7563ba9ec17
anapaunavas <anapaulahong@gmail.com>
2023-11-05 21:49:58 -0600

    firebase
    crear el proyecto en firebase y conectarlo con android

--- EventZezziApp/build.gradle.kts
+++ EventZezziApp/build.gradle.kts
@@ -2,4 +2,20 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.android.application")
+    id("com.google.gms.google-services")
+}
+
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 }
